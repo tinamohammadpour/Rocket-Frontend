69aca9eae73d0da6677ea4c041aedf8628b74857
Tina.2017 <tina.mpmp2017@gmail.com>
2026-08-14 17:58:17 +0330

    feat: added signup zod schema

--- src/schemas/authSchema.ts
+++ src/schemas/authSchema.ts
@@ -1,10 +1,21 @@
 import { z } from 'zod';
 
-export const loginSchema = z.object({
-  password: z
+export const signupSchema = z.object({
+  phonenumber: z
     .string()
-    .min(1, 'this field cant be empty')
-    .min(8, 'password must be atleast 8 characters'),
+    .min(1, 'پر کردن این فیلد الزامی است')
+    .length(11, 'شماره تلفن باید ۱۱ رقم باشد')
+    .regex(/^09/, 'شماره تلفن باید با ۰۹ شروع شود')
+    .regex(/^\d+$/, 'شماره تلفن فقط می‌تواند شامل اعداد باشد'),
+
+  username: z
+    .string()
+    .min(1, 'پر کردن این فیلد الزامی است')
+    .min(5, 'نام کاربری باید حداقل ۵ کاراکتر باشد')
+    .max(25, 'نام کاربری باید حداکثر ۲۵ کاراکتر باشد')
+    .regex(/^[a-zA-Z0-9_]+$/, 'فقط حروف انگلیسی، اعداد و زیرخط (_) مجاز است')
+    .regex(/[a-zA-Z]/, 'نام کاربری باید حداقل یک حرف انگلیسی داشته باشد')
+    .refine((val) => !val.includes(' '), 'نام کاربری نباید دارای فاصله باشد'),
 });
 
-export type loginSchemaType = z.infer<typeof loginSchema>;
+export type signupSchemaType = z.infer<typeof signupSchema>;
